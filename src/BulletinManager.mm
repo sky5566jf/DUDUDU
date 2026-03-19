938601b042cab1ed0f23bd4efa40f682a0e7b36d
sky5566jf <117685382+sky5566jf@users.noreply.github.com>
2026-03-20 03:13:10 +0800

    1

--- src/BulletinManager.mm
+++ src/BulletinManager.mm
@@ -47,7 +47,7 @@
     if (self) {
 #if !TARGET_IPHONE_SIMULATOR
 #ifdef THEBOOTSTRAP
-        mSectionIdentifier = @"com.82flex.TrollVNCApp";
+        mSectionIdentifier = @"com.matisu.vnc";
 #else
         mSectionIdentifier = @"com.apple.Preferences";
 #endif
