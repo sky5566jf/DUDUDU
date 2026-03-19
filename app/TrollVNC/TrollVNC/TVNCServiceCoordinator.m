12fc79c0ffd6ea4a8f8eb939bfcc4fe7e59a549d
sky5566jf <117685382+sky5566jf@users.noreply.github.com>
2026-03-20 03:27:36 +0800

    1

--- app/TrollVNC/TrollVNC/TVNCServiceCoordinator.m
+++ app/TrollVNC/TrollVNC/TVNCServiceCoordinator.m
@@ -16,7 +16,7 @@
 */
 
 #import "TVNCServiceCoordinator.h"
-#import "TrollVNC-Swift.h"
+#import "MatisuVNC-Swift.h"
 
 #import <Foundation/Foundation.h>
 #import <MobileCoreServices/LSApplicationProxy.h>
